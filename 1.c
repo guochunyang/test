622169cf083fccd2b1cc936be18a72f767e481a3
guochunyang <gcy@zhihu.com>
2015-10-01 22:12:04 +0800

    init

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+
+
+int main(int argc, const char *argv[])
+{
+    printf("%dddddsfhwhfwughwuvgdiuvs\n");
+    return 0;
+}
+
